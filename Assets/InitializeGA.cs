8de6fdb0ea40923a3975596eaacbbc4e58a9d523
TeamanZz <a.yasinko@mail.ru>
2021-10-19 18:41:19 +0300

    All Ok

---
+++ Assets/InitializeGA.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+
+public class InitializeGA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
